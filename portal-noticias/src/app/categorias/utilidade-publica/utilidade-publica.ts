979d3db25f57a17b80c237e3f083916c29487a3a
Jackeline <jackeline.delima17@gmail.com>
2026-06-13 17:56:47 -0300

    adicionando conteudo de utilidades-publicas
    Adicionando o conteudo do .ts para utilidade-publica funcionar.
    
    Adicionando o conteudo basico de utilidade-publica.html, com o conteudo basico das 3 noticias

---
+++ portal-noticias/src/app/categorias/utilidade-publica/utilidade-publica.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-utilidade-publica',
+  standalone: true,
+  imports: [],
+  templateUrl: './utilidade-publica.html',
+  styleUrl: './utilidade-publica.scss'
+})
+export class UtilidadePublicaComponent {}
