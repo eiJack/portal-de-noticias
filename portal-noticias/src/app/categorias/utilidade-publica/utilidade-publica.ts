803f891c846a480a5d4fed8b0d9cc71afce22680
Jackeline <jackeline.delima17@gmail.com>
2026-06-13 20:08:03 -0300

    alterando css e html de utilidades publicas
    Arrumando o css para deixar a pagina de utilidades publicas mais agradavel de ver. Atualmente esta simples.
    
    Adicionando routerLink e o botão para permitir navegação para as noticias dessa categoria.

--- portal-noticias/src/app/categorias/utilidade-publica/utilidade-publica.ts
+++ portal-noticias/src/app/categorias/utilidade-publica/utilidade-publica.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-utilidade-publica',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './utilidade-publica.html',
   styleUrl: './utilidade-publica.scss'
 })
