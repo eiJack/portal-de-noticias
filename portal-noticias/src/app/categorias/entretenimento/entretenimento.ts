a237c002ae09235d3e53d100a0b0b17a0d29dc91
Jackeline <jackeline.delima17@gmail.com>
2026-06-15 21:25:24 -0300

    adicionando codigos da categoria entretenimento

--- portal-noticias/src/app/categorias/entretenimento/entretenimento.ts
+++ portal-noticias/src/app/categorias/entretenimento/entretenimento.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-entretenimento',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './entretenimento.html',
   styleUrl: './entretenimento.scss'
 })
